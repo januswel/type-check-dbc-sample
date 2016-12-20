1bc6390a9504e29259c8532fd414a4fdb2d45553
janus_wel <janus.wel.3@gmail.com>
2016-12-20 14:24:26 +0900

    :art: add type annotation

--- src/lib/awesome.js
+++ src/lib/awesome.js
@@ -10,7 +10,7 @@ export function sum(...n: Array<number>): number {
     return n.reduce((previous, current) => previous + current, 0)
 }
 
-export function zeroPadding(n: number, digit: number = 8) {
+export function zeroPadding(n: number, digit: number = 8): string {
     assert(1 < digit)
     const zeros = Array(digit - 1).fill('0').join('')
     return `${zeros}${n.toString()}`.slice(-digit)
